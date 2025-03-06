41b2e70326ee01641b030b41fd729728b681f21b
William-Albino <131724457+William-Albino@users.noreply.github.com>
2025-03-06 19:15:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Lab-Quiz/', // Nome exato do repositório no GitHub
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
